2602be9639ac137c71026ba1c6814f592e4605a2
Gustavo Camacho <Gusttav28@GMAIL.COM>
2025-06-12 01:38:48 -0600

    updating some files in order to putting ready to deploy

--- unitconvert/src/app/components/Navbar.jsx
+++ unitconvert/src/app/components/Navbar.jsx
@@ -7,7 +7,6 @@ export default function Navbar() {
             <Link href="/Length" className="block px-4 py-2 items-center hover:bg-gray-900 hover:rounded-full hover:scale-105 transition-transform font-bold">Length</Link>
             <Link href="/Weight" className="block px-4 py-2 items-center hover:bg-gray-900 hover:rounded-full hover:scale-105 transition-transform font-bold">Weight</Link>
             <Link href="/Tempeture" className="block px-4 py-2 items-center hover:bg-gray-900 hover:rounded-full hover:scale-105 transition-transform font-bold">Tempeture</Link>
-            {/* <Link href="/Test" className="block px-4 py-2 items-center hover:bg-gray-900 hover:rounded-full hover:scale-105 transition-transform font-bold">Test</Link> */}
         </nav>
     )
 } 
